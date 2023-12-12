208c52b23f4c28148e15b1f9d513544673dc9a9b
jhon.mantilla <jhon.mantilla@sasaconsultoria.com.co>
2023-12-11 21:21:58 -0500

    cambios

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,13 +13,14 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         \App\Models\User::factory()->create();
-        \App\Models\Programs::factory(20)->create();
-        \App\Models\Students::factory(50)->create();
+        \App\Models\Programs::factory(50)->create();
+        \App\Models\Students::factory(10)->create();
         \App\Models\Employees::factory(5)->create();
         \App\Models\Company::factory(1)->create();
         \App\Models\Certificate::factory(150)->create();
         \App\Models\Course::factory(50)->create();
         \App\Models\ProgramsCourses::factory(50)->create();
+        \App\Models\CertificatesCourses::factory(150)->create();
 
 
         // \App\Models\User::factory()->create([
